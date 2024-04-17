5ad55044fdf9a5247fd9ad20ddcff789224ff670
MatteoPas93 <pascucci-matteooutlook.it>
2024-04-17 15:41:04 +0200

    pagina dettagli

--- frontend/src/components/Detail/DestinationDetails.jsx
+++ frontend/src/components/Detail/DestinationDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import './detail.css'
 
 const DestinationDetail = () => {
   const { id } = useParams();
@@ -43,21 +44,24 @@ const DestinationDetail = () => {
 
   return (
     <>
-     
-        <div className="row">
-          <h2> {destination.travel_location} </h2>
-          <div>
-          <img src={destination.cover_image} alt="location" />
-          <p> {destination.description} </p>
-          </div>
-          <div>
-          {destination.images_location.map((image, index) => {
-           return <img key={index} src={image} alt={`image_${index}`} />
-          })}
-          <p> {destination.main_attractions} </p>
-          </div>
+      <div className="row container-detail flex-column align-items-center flex-wrap w-100">
+        <div className="title-location col-md-6 text-center">
+        <h2> {destination.travel_location} </h2>
         </div>
-      
+        <div className="col-lg-6">
+          <img className="w-100 mb-2" src={destination.cover_image} alt="location" />
+          <p className="text"> {destination.description} </p>
+        </div>
+        <div className="col-lg-10 row gap-2 mb-2 flex-wrap justify-content-center">
+          {destination.images_location.map((image, index) => {
+            return <div key={index} className="col-lg-3 images-location"> <img src={image} alt={`image_${index}`} /></div>;
+          })}
+          
+          </div>
+          <div className="col-md-10">
+          <p className="text"> {destination.main_attractions} </p>
+        </div>
+      </div>
     </>
   );
 };
